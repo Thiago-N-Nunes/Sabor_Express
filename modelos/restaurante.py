f1cdda47060eac5a04f2ac08cb3ce1cf4f791733
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-05-14 20:47:49 -0300

    Criando métodos

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,24 @@
 #Classe = Define um conjunto de caracteristicas que selfs pertencentes a ela têm em comum
-class restaurante:
+class Restaurante:
+    restaurantes = []
     def __init__(self,nome,categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self)
+    
+    #transformação de espaços de memória em string     
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 #Criação de self
-restaurante_muhaui = restaurante('Muhaui', 'Vegano')
+restaurante_muhaui = Restaurante('Muhaui', 'Vegano')
+restaurante_oviedo = Restaurante('Oviedo', 'Ibérico')
 #Atributo de instancia
 print(restaurante_muhaui)
 
+Restaurante.listar_restaurantes()
