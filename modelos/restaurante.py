0cf24eefe97f06fa1cad29dd3ba8ecb886c4b22a
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-05-14 20:24:31 -0300

    Implementando métodos especiais

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,10 +1,14 @@
-#Classe = Define um conjunto de caracteristicas que objetos pertencentes a ela têm em comum
+#Classe = Define um conjunto de caracteristicas que selfs pertencentes a ela têm em comum
 class restaurante:
     def __init__(self,nome,categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
-#Criação de Objeto
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+#Criação de self
 restaurante_muhaui = restaurante('Muhaui', 'Vegano')
 #Atributo de instancia
-print(vars(restaurante_muhaui ))
+print(restaurante_muhaui)
+
