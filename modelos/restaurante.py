fb207e3cc18a966aee709e8fc913343fb054424c
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-05-15 21:52:49 -0300

    Utilização do property

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -4,16 +4,22 @@ class Restaurante:
     def __init__(self,nome,categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
     
     #transformação de espaços de memória em string
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
+#Criação de método 
     def listar_restaurantes():
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria do Restaurante'.ljust(25)} | {'Status do Restaurante'.ljust(25)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
+#Modificação de como o atributo é lido
+    @property
+    def ativo(self):
+        return '✔️' if self._ativo else '✖️'
 
 #Criação de self
 restaurante_muhaui = Restaurante('Muhaui', 'Vegano')
