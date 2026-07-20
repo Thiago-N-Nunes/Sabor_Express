46e9cef38ccd3cfb9a8c3580960b6cb194f511ea
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-07-20 19:01:23 -0300

    implementação de metodos e propriedades / resolução de exercicios

--- modelos/restaurante_novo.py
+++ modelos/restaurante_novo.py
@@ -1,14 +1,30 @@
 #Criação de classe
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
-    
-restaurante_zuko = Restaurante()
-restaurante_zuko.nome = 'Zuko Sushi'
-restaurante_zuko.categoria = 'Asiatico'
-restaurante_soka = Restaurante()
+    restaurantes = [] #Essa lista vai receber todos os restaurantes que forem cadastrados
 
-restaurantes = [restaurante_zuko, restaurante_soka]
+    def __init__(self,nome, categoria):#Cria um 'Criador' - uma cadeia de caracteristicas que será tratado como obrigatória para os objetos da classe 
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self) # Toda vez que um restaurante for criado, será armazenado na lista 'restaurantes'
+        
+#    def __str__(self): #Transforma a maneira de visualizar os objetos/atributos
 
-print(restaurante_zuko.ativo)
+
+    @classmethod #Método para a classe
+    def listar_restaurantes(cls):# metodo que mostra os restaurantes
+        print(f'{'RESTAURANTE'.ljust(25)} | {'CATEGORIA'.ljust(25)} | {'SITUAÇÃO'.ljust(25)}')
+        for restaurante in cls.restaurantes: #Loop para percorrer a lista de restaurantes
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
+
+    @property
+    def ativo(self):
+        return '✅' if self._ativo else '✖'
+    def alternar_estado(self):#Método para os objetos da classe
+        self._ativo = not self._ativo
+
+restaurante_zuko = Restaurante('zuko sushi', 'Asiatico')
+restaurante_zuko.alternar_estado()
+restaurante_Torf = Restaurante('torf habib', 'Arabe')
+     
+Restaurante.listar_restaurantes()
