ae60b0488970376c8ed98a4dee68e4d4c32009ac
Thiago Nunes <thiagonesi.cntt@hotmail.com>
2026-07-21 12:20:18 -0300

    projeto inicial finalizado

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from modelos.restaurante_novo import Restaurante
 restaurante1 = Restaurante('MilkMu', 'Sorveteria')
 restaurante2 = Restaurante('Bruscheta da Nona', 'Cantina Italiana')
 restaurante3 = Restaurante('Hayumi Sushi', 'Comida Japonesa')
+restaurante3.receber_avaliacao('Thiago', 10)
+restaurante3.receber_avaliacao('Cauã', 5)
+restaurante3.receber_avaliacao('Giovanna', 8)
+
 def main():
     Restaurante.listar_restaurantes()
 if __name__ == '__main__':
--- modelos/restaurante_novo.py
+++ modelos/restaurante_novo.py
@@ -16,12 +16,28 @@ class Restaurante:
 
     @classmethod #Método para a classe
     def listar_restaurantes(cls):# metodo que mostra os restaurantes
-        print(f'{'RESTAURANTE'.ljust(25)} | {'CATEGORIA'.ljust(25)} | {'SITUAÇÃO'.ljust(25)}')
+        print(f'{'RESTAURANTE'.ljust(20)} | {'CATEGORIA'.ljust(20)} | {'AVALIAÇÃO'.ljust(20)} |{'SITUAÇÃO'.ljust(20)}')
         for restaurante in cls.restaurantes: #Loop para percorrer a lista de restaurantes
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo.ljust(25)}')
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {str(restaurante.media_avaliacao).ljust(20)} | {restaurante.ativo.ljust(20)}')
 
     @property
     def ativo(self):
         return '✅' if self._ativo else '✖'
+    
     def alternar_estado(self):#Método para os objetos da classe
         self._ativo = not self._ativo
+    
+    def receber_avaliacao(self,cliente,nota):
+        if 0 < nota <= 5:
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
+    
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return '-'
+        #sum(avaliacao._nota for avaliacao in self._avaliacao) = pega apenas as notas da classe avaliação e soma
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)  
+        qtd_notas = len(self._avaliacao)
+        media = round(soma_notas / qtd_notas,1)
+        return media
